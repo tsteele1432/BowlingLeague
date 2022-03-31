c2428e4688a33fd4517a8a4d8bef8e556366921f
tsteele1432 <tsteele842@gmail.com>
2022-03-30 19:20:45 -0600

    Add project files.

---
+++ BowlingLeague/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlingLeagueDbContext _context { get; set; }
+
+        public HomeController(BowlingLeagueDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _context.Bowlers.ToList();
+            return View(blah);
+        }
+
+
+
+        
+    }
+}
---
+++ BowlingLeague/Models/BowlingLeagueDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Models
+{
+    public class BowlingLeagueDbContext : DbContext
+    {
+        public BowlingLeagueDbContext(DbContextOptions<BowlingLeagueDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Bowler> Bowlers { get; set; }
+    }
+}
